1f37c88ffb72bb85276df2549821813f3adc0f3a
Rajesh Mohan <rajeshmohanmj@gmail.com>
2022-10-14 17:46:16 +0530

    adding provider

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {}
+
 resource "github_repository" "example" {
   name        = "example"
   description = "My awesome codebase"
